89800a39c74b83a7410e766f63dc0d388622df29
Nick Vigier <nvigier46@gmail.com>
2024-07-08 08:44:41 +0100

    Update astro.config.ts
    set output to 'hybrid'

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercelServerless from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
   adapter: vercelServerless({
     imageService: true,
   }),
